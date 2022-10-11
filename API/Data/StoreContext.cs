7d0cf3c4b7cccb7a08b8af1390b19d38c34fa71e
Alens <alenskupnjak@yahoo.com>
2022-10-11 17:02:41 +0200

    L12

---
+++ API/Data/StoreContext.cs
@@ -0,0 +1,12 @@
+﻿
+namespace API.Data
+{
+  public class StoreContext : Microsoft.EntityFrameworkCore.DbContext
+  {
+    public StoreContext(Microsoft.EntityFrameworkCore.DbContextOptions options) : base(options)
+    {
+    }
+
+    public Microsoft.EntityFrameworkCore.DbSet<API.Entities.Product> ProductsTBL { get; set; }
+  }
+}
