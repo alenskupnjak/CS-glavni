66676b751c073f724a804ec8fd29613223ef960a
Alens <alenskupnjak@yahoo.com>
2022-11-02 16:36:24 +0100

    L124

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,10 +1,12 @@
 ﻿
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-  public class StoreContext : DbContext
+  public class StoreContext : IdentityDbContext<User>
   {
     public StoreContext(DbContextOptions options) : base(options)
     {
@@ -12,5 +14,18 @@ namespace API.Data
 
     public DbSet<Product> ProductsTBL { get; set; }
     public DbSet<Basket> BasketsTBL { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      base.OnModelCreating(builder);
+
+
+      // Dodaje Role na bazu....
+      builder.Entity<IdentityRole>()
+          .HasData(
+              new IdentityRole { Name = "Member", NormalizedName = "MEMBER" },
+              new IdentityRole { Name = "Admin", NormalizedName = "ADMIN" }
+          );
+    }
   }
 }
